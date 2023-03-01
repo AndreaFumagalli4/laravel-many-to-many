6a32ee7e958595c6ac18e0a36fd2601d3b5baac6
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-03-01 15:50:25 +0100

    added technologies seeder

---
+++ database/seeders/TechnologiesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TechnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['HTML5', 'CSS3', 'SCSS', 'Bootstrap 5.2', 'JS', 'Vue 3', 'PHP8', 'Laravel 9', 'Nodejs'];
+        foreach ($technologies as $technologyName) {
+            $technology = new Technology();
+            $technology->name = $technologyName;
+            $technology->save();
+        }
+    }
+}
+
